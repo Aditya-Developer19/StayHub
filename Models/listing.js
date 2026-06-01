edb192ead4cca97e127f32d30b250abcb1336c9d
Aditya-Developer19 <chauhanaadi015@gmail.com>
2026-06-02 01:30:58 +0530

    models and routes

---
+++ Models/listing.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title:{
+        type: String,
+        required: true
+    },
+    description:{
+        type: String
+    },
+    image:{
+        type: String,
+        set: (v) => v === '' ? 'https://via.https://plus.unsplash.com/premium_vector-1721890983105-625c0d32045f?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.com/150' : v
+    },
+    price:{
+        type: Number
+    },
+    location:{
+        type: String
+    },
+    country:{
+        type: String
+    }
+
+});
+const Listing = mongoose.model('Listing', listingSchema);
+module.exports = Listing;
--- app.js
+++ app.js
@@ -3,10 +3,30 @@ const app = express();
 const port = 8080;
 const mongoose = require('mongoose');
 const ejs = require('ejs');
+const Listing = require('./Models/listing');
 
 main().then(() => {
   console.log('Connected to MongoDB');
 }).catch(err => console.log(err));
+
+
+app.get('/', (req, res) => {
+    res.send("app is running");
+});
+
+app.get('/tesListings', async(req, res) => {
+    let sampleListings = new Listing({
+        title: "Cozy Apartment in the Heart of the City",
+        description: "A charming and cozy apartment located in the heart of the city, perfect for exploring local attractions and enjoying vibrant nightlife.",
+        price: 7000,
+        location: "Downtown",
+        country: "USA"
+    });
+    await sampleListings.save();
+    res.send("Sample listing created and saved to database");
+    console.log("Listing saved to database");
+});
+
 
 async function main(){
     await mongoose.connect('mongodb://127.0.0.1:27017/StayHub');
